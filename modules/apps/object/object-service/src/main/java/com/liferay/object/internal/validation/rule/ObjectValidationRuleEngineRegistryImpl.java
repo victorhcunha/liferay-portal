0b2c848beab7b28e08e937d6027f009a7b277f71
Mateus Santana <mateus.santana@liferay.com>
2023-07-25 21:21:50 -0300

    LPS-190412 Filter scoped Object Validations Client Extensions

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/validation/rule/ObjectValidationRuleEngineRegistryImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/validation/rule/ObjectValidationRuleEngineRegistryImpl.java
@@ -5,13 +5,16 @@
 
 package com.liferay.object.internal.validation.rule;
 
+import com.liferay.object.scope.CompanyScoped;
+import com.liferay.object.scope.ObjectDefinitionScoped;
 import com.liferay.object.validation.rule.ObjectValidationRuleEngine;
 import com.liferay.object.validation.rule.ObjectValidationRuleEngineRegistry;
 import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapper;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.util.ListUtil;
 
-import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import org.osgi.framework.BundleContext;
@@ -29,14 +32,56 @@ public class ObjectValidationRuleEngineRegistryImpl
 
 	@Override
 	public ObjectValidationRuleEngine getObjectValidationRuleEngine(
-		String key) {
+		String name) {
 
-		return _serviceTrackerMap.getService(key);
+		return _serviceTrackerMap.getService(name);
 	}
 
 	@Override
-	public List<ObjectValidationRuleEngine> getObjectValidationRuleEngines() {
-		return new ArrayList(_serviceTrackerMap.values());
+	public List<ObjectValidationRuleEngine> getObjectValidationRuleEngines(
+		long companyId, String objectDefinitionName) {
+
+		Collection<ObjectValidationRuleEngine>
+			objectValidationRuleEnginesCollection = _serviceTrackerMap.values();
+
+		return ListUtil.sort(
+			ListUtil.filter(
+				ListUtil.fromCollection(objectValidationRuleEnginesCollection),
+				objectValidationRuleEngine -> {
+					boolean allowed = true;
+
+					if (objectValidationRuleEngine instanceof CompanyScoped) {
+						CompanyScoped objectValidationRuleEngineCompanyScoped =
+							(CompanyScoped)objectValidationRuleEngine;
+
+						allowed =
+							objectValidationRuleEngineCompanyScoped.
+								isAllowedCompany(companyId);
+					}
+
+					if (objectValidationRuleEngine instanceof
+							ObjectDefinitionScoped) {
+
+						ObjectDefinitionScoped
+							objectValidationRuleEngineObjectDefinitionScoped =
+								(ObjectDefinitionScoped)
+									objectValidationRuleEngine;
+
+						allowed =
+							objectValidationRuleEngineObjectDefinitionScoped.
+								isAllowedObjectDefinition(objectDefinitionName);
+					}
+
+					return allowed;
+				}),
+			(ObjectValidationRuleEngine objectValidationRuleEngine1,
+			 ObjectValidationRuleEngine objectValidationRuleEngine2) -> {
+
+				String name1 = objectValidationRuleEngine1.getName();
+				String name2 = objectValidationRuleEngine2.getName();
+
+				return name1.compareTo(name2);
+			});
 	}
 
 	@Activate
