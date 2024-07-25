b07cdd74a85032156d3a596b7b74e652acbd1c27
victorhcunha <victor.cunha@liferay.com>
2024-07-25 08:44:54 -0300

    LPD-28287 Deprecate unused commerce-api classes

--- modules/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/util/comparator/CommerceOrderTotalComparator.java
+++ modules/apps/commerce/commerce-api/src/main/java/com/liferay/commerce/util/comparator/CommerceOrderTotalComparator.java
@@ -12,7 +12,9 @@ import java.math.BigDecimal;
 
 /**
  * @author Andrea Di Giorgi
+ * @deprecated As of Cavanaugh (7.4.x), with no direct replacement
  */
+@Deprecated
 public class CommerceOrderTotalComparator
 	extends OrderByComparator<CommerceOrder> {
 
