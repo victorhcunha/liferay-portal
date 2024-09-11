86d6ac3bb227260ebe8e63c9c678fe91c3678fd0
victorhcunha <victor.cunha@liferay.com>
2024-09-11 12:41:08 -0300

    LPD-35966 Change class to use comparator

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/display/context/comparator/SiteInitializerNameComparator.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/display/context/comparator/SiteInitializerNameComparator.java
@@ -5,23 +5,15 @@
 
 package com.liferay.site.admin.web.internal.display.context.comparator;
 
-import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.site.admin.web.internal.util.SiteInitializerItem;
+
+import java.util.Comparator;
 
 /**
  * @author Marco Leo
  */
 public class SiteInitializerNameComparator
-	extends OrderByComparator<SiteInitializerItem> {
-
-	public SiteInitializerNameComparator() {
-		this(false);
-	}
-
-	public SiteInitializerNameComparator(boolean ascending) {
-		_ascending = ascending;
-	}
-
+	implements Comparator<SiteInitializerItem> {
 	@Override
 	public int compare(
 		SiteInitializerItem siteInitializerItem1,
@@ -30,20 +22,7 @@ public class SiteInitializerNameComparator
 		String name1 = siteInitializerItem1.getName();
 		String name2 = siteInitializerItem2.getName();
 
-		int value = name1.compareToIgnoreCase(name2);
-
-		if (_ascending) {
-			return value;
-		}
-
-		return -value;
+		return name1.compareToIgnoreCase(name2);
 	}
-
-	@Override
-	public boolean isAscending() {
-		return _ascending;
-	}
-
-	private final boolean _ascending;
 
 }
