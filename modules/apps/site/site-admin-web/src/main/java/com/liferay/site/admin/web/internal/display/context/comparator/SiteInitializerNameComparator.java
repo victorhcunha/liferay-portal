79dadba3eabe35888b0746302af796c30730473e
victorhcunha <victor.cunha@liferay.com>
2024-09-12 17:02:44 -0300

    LPD-35966 Apply SiteInitializerNameComparator usages

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/display/context/comparator/SiteInitializerNameComparator.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/display/context/comparator/SiteInitializerNameComparator.java
@@ -14,6 +14,7 @@ import java.util.Comparator;
  */
 public class SiteInitializerNameComparator
 	implements Comparator<SiteInitializerItem> {
+
 	@Override
 	public int compare(
 		SiteInitializerItem siteInitializerItem1,
