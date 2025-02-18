6bec8dee5f84a3a5849ce54f16d7ef16b89a728e
Ivica Cardic <ivica.cardic@liferay.com>
2025-02-18 15:29:53 +0100

    LPD-49221 - Stripe tax sample connector app

---
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-tax-engine/src/main/java/com/liferay/sample/CalculateTaxRestController.java
@@ -0,0 +1,48 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sample;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.json.JSONObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ivica Cardic
+ */
+@RequestMapping("/calculate-tax")
+@RestController
+public class CalculateTaxRestController extends BaseRestController {
+
+	@PostMapping
+	public ResponseEntity<String> post(
+			@AuthenticationPrincipal Jwt jwt, @RequestBody String json)
+		throws Exception {
+
+		log(jwt, _log, json);
+
+
+		return new ResponseEntity<>(
+			new JSONObject(
+			).put(
+				"amount", 13.90
+			).put(
+				"errorMessages", (String)null
+			).toString(),
+			HttpStatus.OK);
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		CalculateTaxRestController.class);
+
+}
