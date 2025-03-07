e53eeb8cd79f35c3e6d5b87f9bcb0ab5b264f258
Iván Zaera Avellón <ivan.zaera@liferay.com>
2025-03-07 09:02:57 +0100

    LPD-50755 liferay-page-management - modules/apps/layout/layout-content-page-editor-web

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/usePageContents.ts
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/usePageContents.ts
@@ -3,7 +3,8 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {State, useLiferayState} from '@liferay/frontend-js-state-web';
+import {State} from '@liferay/frontend-js-state-web';
+import {useLiferayState} from '@liferay/frontend-js-state-web/react';
 import {useEffect} from 'react';
 
 import {useSelector} from '../contexts/StoreContext';
