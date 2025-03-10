b722d66cbed0ab04da8355837e53077baa8595f7
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2025-03-10 17:31:35 +0100

    LPD-50584 Add logic for localizable input

--- modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/friendly-url-input/index.js
+++ modules/apps/fragment/fragment-collection-contributor/fragment-collection-contributor-inputs/src/main/resources/com/liferay/fragment/collection/contributor/inputs/dependencies/friendly-url-input/index.js
@@ -6,6 +6,26 @@ function main() {
 	if (layoutMode === 'edit' && inputElement) {
 		inputElement.setAttribute('disabled', true);
 	}
+	else {
+		if (Liferay.FeatureFlags['LPD-37927']) {
+			import('@liferay/fragment-impl').then(
+				({registerLocalizedInput}) => {
+					const {onChange} = registerLocalizedInput({
+						defaultLanguageId: themeDisplay.getDefaultLanguageId(),
+						initialValues: input.valueI18n,
+						inputElement,
+						inputName: input.name,
+						localizationInputsContainer: inputElement.parentNode,
+						namespace: fragmentNamespace,
+					});
+
+					inputElement.addEventListener('change', (event) => {
+						onChange(event.target.value);
+					});
+				}
+			);
+		}
+	}
 }
 
 main();
