7179bfe48a9ee67e70a9c350fc7cd65de7f1403e
Mikel Lorza <mikel.lorza@liferay.com>
2025-03-12 20:54:34 +0100

    LPD-51094 Inline
    (cherry picked from commit af4051c20d7c8412c5358b88ce2aca748d686881)

--- modules/apps/sharing/sharing-notifications/src/main/java/com/liferay/sharing/notifications/internal/service/NotificationsSharingEntryLocalServiceWrapper.java
+++ modules/apps/sharing/sharing-notifications/src/main/java/com/liferay/sharing/notifications/internal/service/NotificationsSharingEntryLocalServiceWrapper.java
@@ -303,11 +303,9 @@ public class NotificationsSharingEntryLocalServiceWrapper
 
 		try {
 			if (sharingEntry.getToUserId() > 0) {
-				User user = _userLocalService.getUser(
-					sharingEntry.getToUserId());
-
 				_sendNotificationEvent(
-					sharingEntry, notificationType, serviceContext, user);
+					sharingEntry, notificationType, serviceContext,
+					_userLocalService.getUser(sharingEntry.getToUserId()));
 
 				return;
 			}
