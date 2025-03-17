4a61cd8562bfbeaeaf7cc074a76a814e9c85b5c6
Lucas Emanuel <lemmg@cin.ufpe.br>
2025-03-17 15:41:56 -0300

    LRSD-8765 Refactor Navigation Menu

---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/public-sites-navigation-dropdown/index.js
@@ -0,0 +1,172 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+const editMode = layoutMode === 'edit';
+const menu = fragmentElement.querySelector('.dropdown-fragment-menu');
+const toggle = fragmentElement.querySelector('.dropdown-fragment-toggle');
+
+const menuDropdownCaret = toggle.querySelector('.menu-dropdown-caret');
+const toggleEditable = toggle.querySelector('[data-lfr-editable-id]');
+const regularMenuWidth = 240;
+const withinMasterLayout = fragmentElement.parentElement.classList.contains(
+	'page-editor__fragment-content--master'
+);
+
+let alignMenuInterval;
+
+function alignMenu() {
+	const toggleRect = toggle.getBoundingClientRect();
+	const parent =
+		document.querySelector('.page-editor__layout-viewport__resizer') ||
+		document.body;
+	const parentRect = parent.getBoundingClientRect();
+	const wrapperRect = document
+		.querySelector('#wrapper')
+		?.getBoundingClientRect();
+	const isRTL =
+		Liferay.Language.direction?.[themeDisplay?.getLanguageId()] === 'rtl';
+
+	menu.style.top = `${toggleRect.bottom}px`;
+
+	if (configuration.panelType === 'mega-menu') {
+		menu.style.left = `${parentRect.left}px`;
+		menu.style.width = `${parentRect.width}px`;
+	}
+	else if (configuration.panelType === 'regular') {
+		menu.style.width = `${regularMenuWidth}px`;
+
+		if (
+			toggleRect.left + regularMenuWidth >= window.innerWidth ||
+			(wrapperRect &&
+				toggleRect.left + regularMenuWidth >= wrapperRect.width)
+		) {
+			menu.style.right = `${window.innerWidth - toggleRect.right}px`;
+		}
+		else {
+			menu.style.right = null;
+		}
+
+		if (isRTL && toggleRect.right - regularMenuWidth < 0) {
+			menu.style.left = `${toggleRect.left}px`;
+		}
+	}
+	else if (configuration.panelType === 'full-width') {
+		menu.style.width = `${fragmentElement.getBoundingClientRect().width}px`;
+	}
+}
+
+function handleBodyClick(event) {
+	if (!toggle.isConnected) {
+		document.body.removeEventListener('click', handleBodyClick);
+
+		return;
+	}
+
+	if (
+		isShown() &&
+		!toggle.contains(event.target) &&
+		!menu.contains(event.target)
+	) {
+		toggleMenu();
+	}
+}
+
+function handleDropdownHover() {
+	if (!isShown()) {
+		toggleMenu();
+	}
+}
+
+function handleDropdownLeave() {
+	if (isShown()) {
+		toggleMenu();
+	}
+}
+
+function handleToggleClick(event) {
+	if (!toggleEditable.contains(event.target) || !editMode) {
+		toggleMenu();
+	}
+}
+
+function handleWindowEvent() {
+	if (!toggle.isConnected) {
+		window.removeEventListener('resize', handleWindowEvent);
+		window.removeEventListener('scroll', handleWindowEvent);
+
+		return;
+	}
+
+	alignMenu();
+}
+
+function isShown() {
+	return toggle.getAttribute('aria-expanded') === 'true';
+}
+
+function menuHasChildren() {
+	const contentElement = editMode
+		? menu.querySelector('lfr-drop-zone')
+		: menu;
+
+	return (
+		contentElement &&
+		!!Array.from(contentElement.firstElementChild.children).filter(
+			(child) => child.tagName !== 'STYLE'
+		).length
+	);
+}
+
+function toggleMenu() {
+	if (!menuHasChildren()) {
+		return;
+	}
+
+	const isMenuVisible = menu.style.display === 'block';
+
+	if (isMenuVisible) {
+		menu.style.display = 'none';
+		toggle.setAttribute('aria-expanded', 'false');
+		menuDropdownCaret.classList.remove('rotated'); // Remove rotação do caret
+
+		window.removeEventListener('resize', handleWindowEvent);
+		window.removeEventListener('scroll', handleWindowEvent);
+
+		clearInterval(alignMenuInterval);
+	}
+	else {
+		menu.style.display = 'block';
+		toggle.setAttribute('aria-expanded', 'true');
+		menuDropdownCaret.classList.add('rotated'); // Adiciona rotação ao caret
+
+		alignMenu();
+
+		window.addEventListener('resize', alignMenu);
+		window.addEventListener('scroll', alignMenu);
+
+		if (editMode && configuration.keepOpen) {
+			alignMenuInterval = setInterval(alignMenu, 1000);
+		}
+	}
+}
+
+function main() {
+	if (configuration.keepOpen && editMode && !withinMasterLayout) {
+		toggleMenu();
+	}
+	else if (configuration.displayOnHover) {
+		toggle.addEventListener('mouseenter', handleDropdownHover);
+		toggle.addEventListener('mouseleave', handleDropdownLeave);
+
+		menu.addEventListener('mouseenter', handleDropdownHover);
+		menu.addEventListener('mouseleave', handleDropdownLeave);
+	}
+	else {
+		toggle.addEventListener('click', handleToggleClick);
+		document.body.addEventListener('click', handleBodyClick);
+	}
+}
+
+main();
---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/liferay-learn/fragments/public-sites-navigation/index.js
@@ -0,0 +1,293 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+/* eslint-disable no-undef */
+
+/**
+ * Consts and DOM Elements
+ */
+
+const inputElements = ['input', 'textarea'];
+
+const fragmentSearchElements = {
+	searchSubmitLink: fragmentElement.querySelector('.search-submit'),
+	searchSubmitURL: fragmentElement.querySelector('.search-submit').href,
+	searchSuggestionItemTemplate: fragmentElement.querySelector(
+		'.suggestions template'
+	),
+	searchSuggestions: fragmentElement.querySelector('.search-suggestions'),
+	searchSuggestionsInput: fragmentElement.querySelector(
+		'.search-suggestions-input'
+	),
+	seeAllResultsLink: fragmentElement.querySelector(
+		'.search-suggestions-see-all-results-text'
+	),
+	suggestions: fragmentElement.querySelector('.suggestions'),
+};
+
+const menuElements = {
+	menuButton: document.querySelector('.cta-menu-hamburguer'),
+	menuIconLines: document.querySelectorAll(
+		'.nav-items-menu-button-icon-line'
+	),
+	menuTextClose: document.querySelector('.menu-button-text-close'),
+	menuTextOpen: document.querySelector('.menu-button-text-open'),
+};
+
+const siteSearchWrapper = document.getElementById('siteSearchWrapper');
+const documentationEducationDropdown = document.querySelector(
+	'.documentation-education-dropdown'
+);
+const searchInput = document.getElementById('searchInput');
+
+let debounceTimer;
+
+/**
+ * Auxiliary Functions
+ */
+
+async function postData(url = '', data = {}) {
+	const response = await Liferay.Util.fetch(url, {
+		body: JSON.stringify(data),
+		credentials: 'include',
+		headers: {
+			'Accept': 'application/json',
+			'Accept-Language': Liferay.ThemeDisplay.getBCP47LanguageId(),
+			'Content-Type': 'application/json',
+			'x-csrf-token': Liferay.authToken,
+		},
+		method: 'POST',
+	});
+
+	return response.json();
+}
+
+function changeFocus() {
+	searchInput.focus();
+}
+
+function checkScreenSize() {
+	if (window.innerWidth <= 1000) {
+		documentationEducationDropdown.classList.add('hide');
+		menuElements.menuTextOpen.style.display = 'inline';
+		menuElements.menuTextClose.style.display = 'none';
+		menuElements.menuButton.classList.remove('open');
+	}
+	else {
+		documentationEducationDropdown.classList.remove('hide');
+		menuElements.menuTextOpen.style.display = 'inline';
+		menuElements.menuTextClose.style.display = 'none';
+		menuElements.menuButton.classList.remove('open');
+		resetMenuIcon();
+	}
+}
+
+function debounce(callback, time) {
+	window.clearTimeout(debounceTimer);
+	debounceTimer = window.setTimeout(callback, time);
+}
+
+function getBreadcrumbFromURL(url) {
+	if (!url) {
+		return '';
+	}
+	url = url
+		.replaceAll('/web/guest/w/', 'home/')
+		.replaceAll('/web/guest/', 'home/')
+		.replaceAll('/', ' / ')
+		.replaceAll('-', ' ');
+	const ancronymList = ['api', 'ccr', 'dxp', 'mvc', ' ui ', 'url'];
+	ancronymList.forEach((word) => {
+		url = url.replace(new RegExp(word, 'ig'), word.toUpperCase());
+	});
+	const excludeWords = ['a', 'and', 'of', 'the', 'to', 'via'];
+
+	return url
+		.split(' ')
+		.map((word, i) =>
+			excludeWords.includes(word) && i !== 0
+				? word
+				: word.charAt(0).toUpperCase() + word.slice(1)
+		)
+		.join(' ');
+}
+
+function performSearch(query) {
+	const postDataURL = `/o/portal-search-rest/v1.0/suggestions?currentURL=${
+		window.location.href
+	}&destinationFriendlyURL=/search&groupId=${Liferay.ThemeDisplay.getScopeGroupId()}&plid=${Liferay.ThemeDisplay.getPlid()}&scope=this-site&search=${query}`;
+	postData(postDataURL, [
+		{
+			attributes: {
+				includeAssetSearchSummary: true,
+				includeassetURL: true,
+				sxpBlueprintId: configuration.searchBlueprintId,
+			},
+			contributorName: 'sxpBlueprint',
+			displayGroupName: 'Public Nav Search Recommendations',
+			size: '3',
+		},
+	])
+		.then((data) => {
+			if (data?.items?.[0]) {
+				const items = JSON.parse(JSON.stringify(data.items[0]));
+				fragmentSearchElements.searchSuggestions.innerHTML = '';
+				const searchTermRegExp = new RegExp('(' + query + ')', 'gi');
+				items.suggestions.forEach((suggestion) => {
+					const suggestionLink = document.importNode(
+						fragmentSearchElements.searchSuggestionItemTemplate.content.querySelector(
+							'a'
+						),
+						true
+					);
+					const assetURL = suggestion.attributes.assetURL.replace(
+						/\?.*$/,
+						''
+					);
+					suggestionLink.href = assetURL;
+					const suggestionTitle = suggestionLink.querySelector(
+						'.search-suggestion-item-title'
+					);
+					suggestionTitle.textContent = suggestion.text;
+					const suggestionContent = suggestionLink.querySelector(
+						'.search-suggestion-item-content'
+					);
+					let contentText = suggestion.attributes.assetSearchSummary;
+
+					if (contentText) {
+						contentText = contentText.substring(0, 500);
+						suggestionContent.innerHTML = contentText.replace(
+							searchTermRegExp,
+							`<b>$1</b>`
+						);
+					}
+
+					const suggestionURL = suggestionLink.querySelector(
+						'.search-suggestion-item-link'
+					);
+					suggestionURL.textContent = getBreadcrumbFromURL(assetURL);
+					fragmentSearchElements.searchSuggestions.appendChild(
+						suggestionLink
+					);
+					fragmentSearchElements.suggestions.classList.add(
+						'search-results-found'
+					);
+				});
+			}
+			else {
+				fragmentSearchElements.suggestions.classList.remove(
+					'search-results-found'
+				);
+			}
+			fragmentSearchElements.suggestions.classList.remove(
+				'loading-search',
+				'search-error'
+			);
+		})
+		.catch(() => {
+			fragmentSearchElements.suggestions.classList.add('search-error');
+		});
+}
+
+function resetMenuIcon() {
+	menuElements.menuIconLines.forEach((line) => {
+		line.style.transform = '';
+		line.style.opacity = '';
+	});
+}
+
+function toggleMenuAnimation() {
+	const isOpen = menuElements.menuButton.classList.contains('open');
+	menuElements.menuIconLines.forEach((line, index) => {
+		line.style.opacity = index === 1 && isOpen ? '0' : '1';
+		line.style.transform = isOpen
+			? `rotate(${index === 0 ? 45 : -45}deg) translateY(${index === 0 ? 8 : -8}px)`
+			: '';
+	});
+
+	menuElements.menuTextOpen.style.display = isOpen ? 'none' : 'inline';
+	menuElements.menuTextClose.style.display = isOpen ? 'inline' : 'none';
+}
+
+function updateSearch() {
+	fragmentSearchElements.searchSuggestions.innerHTML = '';
+	const searchValue = fragmentSearchElements.searchSuggestionsInput.value;
+	if (searchValue) {
+		fragmentSearchElements.seeAllResultsLink.href =
+			fragmentSearchElements.searchSubmitURL + '?q=' + searchValue;
+		fragmentSearchElements.searchSubmitLink.href =
+			fragmentSearchElements.searchSubmitURL + '?q=' + searchValue;
+		fragmentSearchElements.suggestions.classList.add('performing-search');
+		performSearch(searchValue);
+	}
+	else {
+		fragmentSearchElements.suggestions.classList.remove(
+			'loading-search',
+			'performing-search',
+			'search-error',
+			'search-results-found'
+		);
+	}
+}
+
+/**
+ * Event Listeners
+ */
+
+window.addEventListener('load', () => {
+	if (!navigation?.default?.DropdownProvider) {
+		return;
+	}
+	fragmentSearchElements.searchSuggestionsInput.value = '';
+	[
+		['.account-info', '.account-info', 'menu-open'],
+		['.account-info', '.account-dropdown-nav', 'show', true],
+		['.sites', '.liferay-sites-dropdown', 'show', true],
+		['.sites', '.sites', 'show', true],
+		['.menu-button-group', '.menu-button-group', 'menu-open'],
+		['.menu-button-group', '.tablet-mobile-nav-section', 'menu-open', true],
+		['.language', '.language-selector', 'list-open', true],
+		['.language', '.language-dropdown-list-container', 'list-open', true],
+		['.search-icon, .close-search', '.search-wrapper', 'search-open', true],
+	].forEach((args) => new navigation.default.DropdownProvider(...args));
+});
+
+window.addEventListener('resize', checkScreenSize);
+
+menuElements.menuButton.addEventListener('click', () => {
+	documentationEducationDropdown.classList.toggle('hide');
+	menuElements.menuButton.classList.toggle('open');
+	toggleMenuAnimation();
+});
+
+fragmentSearchElements.searchSuggestionsInput.addEventListener('input', () => {
+	fragmentSearchElements.suggestions.classList.add('loading-search');
+	debounce(updateSearch, 250);
+});
+
+document.getElementById('searchIcon').addEventListener('click', changeFocus);
+
+window.addEventListener('keyup', (event) => {
+	if (event.code === 'Escape' || event.key === 'Escape') {
+		if (!siteSearchWrapper.classList.contains('search-open')) {
+			return;
+		}
+
+		searchInput.blur();
+		siteSearchWrapper.classList.remove('search-open');
+	}
+	if (
+		(event.code === 'Slash' || event.key === '/') &&
+		inputElements.indexOf(document.activeElement.tagName.toLowerCase()) ===
+			-1
+	) {
+		searchInput.focus();
+		if (siteSearchWrapper.classList.contains('search-open')) {
+			return;
+		}
+
+		siteSearchWrapper.classList.add('search-open');
+	}
+});
