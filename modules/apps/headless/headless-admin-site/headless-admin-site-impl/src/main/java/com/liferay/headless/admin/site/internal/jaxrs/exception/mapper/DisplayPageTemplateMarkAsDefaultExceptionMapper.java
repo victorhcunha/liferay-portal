b6034fefc5354bbe5f95ec65f689a8f88fd31399
barbaracabrera <barbara.cabrera@liferay.com>
2025-03-17 14:04:26 +0100

    LPD-49146 Add exception mapper to manage the LayoutPageTemplateEntryDefaultTemplateException for Display Page Templates and Master Pages since both are Layout Page Templates

---
+++ modules/apps/headless/headless-admin-site/headless-admin-site-impl/src/main/java/com/liferay/headless/admin/site/internal/jaxrs/exception/mapper/DisplayPageTemplateMarkAsDefaultExceptionMapper.java
@@ -0,0 +1,59 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.admin.site.internal.jaxrs.exception.mapper;
+
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.exception.LayoutPageTemplateEntryDefaultTemplateException;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import java.util.Objects;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Bárbara Cabrera
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.Site)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.Site.DisplayPageTemplateMarkAsDefaultExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+@Provider
+public class DisplayPageTemplateMarkAsDefaultExceptionMapper
+	extends BaseExceptionMapper
+		<LayoutPageTemplateEntryDefaultTemplateException> {
+
+	@Override
+	protected Problem getProblem(
+		LayoutPageTemplateEntryDefaultTemplateException
+			layoutPageTemplateEntryDefaultTemplateException) {
+
+		String name = "Display Page Template";
+
+		if (Objects.equals(
+				layoutPageTemplateEntryDefaultTemplateException.getType(),
+				LayoutPageTemplateEntryTypeConstants.MASTER_LAYOUT)) {
+
+			name = "Master Page";
+		}
+
+		return new Problem(
+			Response.Status.CONFLICT,
+			StringUtil.replace(
+				layoutPageTemplateEntryDefaultTemplateException.getMessage(),
+				"Layout Page Template Entry", name));
+	}
+
+}
