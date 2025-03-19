db5d036bfb0edd7122d35f9736a417e4c8559096
Lucas Emanuel <lemmg@cin.ufpe.br>
2025-03-19 14:18:31 -0300

    LRSD-8765 sf account-dropdown

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/navigation/fragments/account-dropdown/index.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-site-initializer/site-initializer/fragments/group/navigation/fragments/account-dropdown/index.js
@@ -3,16 +3,17 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-document.addEventListener("click", (event) => {
-    const dropdownContent = document.querySelector(".account-dropdown-content");
-    const dropdownNav = document.querySelector(".account-dropdown-nav");
+document.addEventListener('click', (event) => {
+	const dropdownContent = document.querySelector('.account-dropdown-content');
+	const dropdownNav = document.querySelector('.account-dropdown-nav');
 
-    const checkbox = dropdownNav.querySelector("input[type='checkbox']");
+	const checkbox = dropdownNav.querySelector("input[type='checkbox']");
 
-    if (
-        !dropdownNav.contains(event.target) &&
-        dropdownContent && dropdownContent.style.display !== "none"
-    ) {
-        checkbox.checked = false;
-    }
+	if (
+		!dropdownNav.contains(event.target) &&
+		dropdownContent &&
+		dropdownContent.style.display !== 'none'
+	) {
+		checkbox.checked = false;
+	}
 });
