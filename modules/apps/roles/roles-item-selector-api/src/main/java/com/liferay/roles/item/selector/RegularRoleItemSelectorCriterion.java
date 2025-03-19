601440532bed2ab4d65ba4c93fecef9d754106e3
Alan Huang <alan.huang@liferay.com>
2025-03-19 22:55:32 +0800

    LPD-50859 Move package

--- modules/apps/roles/roles-item-selector-api/src/main/java/com/liferay/roles/item/selector/RegularRoleItemSelectorCriterion.java
+++ modules/apps/roles/roles-item-selector-api/src/main/java/com/liferay/roles/item/selector/RegularRoleItemSelectorCriterion.java
@@ -3,13 +3,11 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.roles.item.selector.organization.role;
-
-import com.liferay.roles.item.selector.BaseRoleItemSelectorCriterion;
+package com.liferay.roles.item.selector;
 
 /**
  * @author Roberto Díaz
  */
-public class OrganizationRoleItemSelectorCriterion
+public class RegularRoleItemSelectorCriterion
 	extends BaseRoleItemSelectorCriterion {
 }
--- modules/apps/roles/roles-item-selector-api/src/main/java/com/liferay/roles/item/selector/SiteRoleItemSelectorCriterion.java
+++ modules/apps/roles/roles-item-selector-api/src/main/java/com/liferay/roles/item/selector/SiteRoleItemSelectorCriterion.java
@@ -3,9 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.roles.item.selector.site.role;
-
-import com.liferay.roles.item.selector.BaseRoleItemSelectorCriterion;
+package com.liferay.roles.item.selector;
 
 /**
  * @author Roberto Díaz
