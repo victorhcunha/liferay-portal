9fa53b0ced16ae659c83e84ba0220e577d88e290
Jorge Avalos <jorge.avalos@liferay.com>
2025-03-19 17:26:34 -0700

    LPD-47268 add DefaultDuplicateRemovalProcess

---
+++ portal-impl/src/com/liferay/portal/upgrade/util/DefaultDuplicateRemovalProcess.java
@@ -0,0 +1,32 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.upgrade.util;
+
+import com.liferay.portal.kernel.upgrade.util.BaseDuplicateRemovalProcess;
+
+/**
+ * @author Jorge Avalos
+ */
+public class DefaultDuplicateRemovalProcess
+	extends BaseDuplicateRemovalProcess {
+
+	public DefaultDuplicateRemovalProcess(String tableName, String columns) {
+		super(tableName, columns, null, null);
+	}
+
+	public DefaultDuplicateRemovalProcess(
+		String tableName, String columns, String orderByColumns,
+		String orderByClause) {
+
+		super(tableName, columns, orderByColumns, orderByClause);
+	}
+
+	@Override
+	protected void doUpgrade() {
+		super.doUpgrade();
+	}
+
+}
