2568530440888a773eec253a2420dad60710d3e8
Mikel Lorza <mikel.lorza@liferay.com>
2025-03-20 17:44:51 +0100

    LPD-51800 Add KeywordQueryContributor for SharingEntry

---
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/internal/search/spi/model/query/contributor/SharingEntryKeywordQueryContributor.java
@@ -0,0 +1,40 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sharing.internal.search.spi.model.query.contributor;
+
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.query.QueryHelper;
+import com.liferay.portal.search.spi.model.query.contributor.KeywordQueryContributor;
+import com.liferay.portal.search.spi.model.query.contributor.helper.KeywordQueryContributorHelper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mikel Lorza
+ */
+@Component(
+	property = "indexer.class.name=com.liferay.sharing.model.SharingEntry",
+	service = KeywordQueryContributor.class
+)
+public class SharingEntryKeywordQueryContributor
+	implements KeywordQueryContributor {
+
+	@Override
+	public void contribute(
+		String keywords, BooleanQuery booleanQuery,
+		KeywordQueryContributorHelper keywordQueryContributorHelper) {
+
+		_queryHelper.addSearchLocalizedTerm(
+			booleanQuery, keywordQueryContributorHelper.getSearchContext(),
+			Field.TITLE, false);
+	}
+
+	@Reference
+	private QueryHelper _queryHelper;
+
+}
