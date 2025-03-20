85b5333bf05dcb1c85a21cabcd01110ac658d755
Mikel Lorza <mikel.lorza@liferay.com>
2025-03-20 10:58:21 +0100

    LPD-51800 Index sharing entry info to use it when we are getting it using headless apis

---
+++ modules/apps/sharing/sharing-service/src/main/java/com/liferay/sharing/internal/search/spi/model/index/contributor/SharingEntryModelDocumentContributor.java
@@ -0,0 +1,63 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sharing.internal.search.spi.model.index.contributor;
+
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.search.spi.model.index.contributor.ModelDocumentContributor;
+import com.liferay.sharing.interpreter.SharingEntryInterpreter;
+import com.liferay.sharing.interpreter.SharingEntryInterpreterProvider;
+import com.liferay.sharing.model.SharingEntry;
+
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mikel Lorza
+ */
+@Component(
+	property = "indexer.class.name=com.liferay.sharing.model.SharingEntry",
+	service = ModelDocumentContributor.class
+)
+public class SharingEntryModelDocumentContributor
+	implements ModelDocumentContributor<SharingEntry> {
+
+	@Override
+	public void contribute(Document document, SharingEntry sharingEntry) {
+		document.addKeyword(Field.CLASS_NAME_ID, sharingEntry.getClassNameId());
+		document.addKeyword(
+			Field.CLASS_PK, String.valueOf(sharingEntry.getClassPK()));
+		document.addDate(Field.CREATE_DATE, sharingEntry.getCreateDate());
+		document.addDate(Field.MODIFIED_DATE, sharingEntry.getModifiedDate());
+		document.addKeyword(
+			"sharingEntryId", String.valueOf(sharingEntry.getSharingEntryId()));
+		document.addLocalizedText(Field.TITLE, _getTitleMap(sharingEntry));
+		document.addKeyword(
+			"toUserGroupId", String.valueOf(sharingEntry.getToUserGroupId()));
+		document.addKeyword(
+			"toUserId", String.valueOf(sharingEntry.getToUserId()));
+		document.addKeyword(Field.USER_ID, sharingEntry.getUserId());
+	}
+
+	private Map<Locale, String> _getTitleMap(SharingEntry sharingEntry) {
+		SharingEntryInterpreter sharingEntryInterpreter =
+			_sharingEntryInterpreterProvider.getSharingEntryInterpreter(
+				sharingEntry);
+
+		if (sharingEntryInterpreter == null) {
+			return null;
+		}
+
+		return sharingEntryInterpreter.getTitleMap(sharingEntry);
+	}
+
+	@Reference
+	private SharingEntryInterpreterProvider _sharingEntryInterpreterProvider;
+
+}
