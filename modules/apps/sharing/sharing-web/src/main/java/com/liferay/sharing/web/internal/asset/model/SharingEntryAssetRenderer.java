fba0a321f1fdca03240000b0b71bdaf747df594b
Mikel Lorza <mikel.lorza@liferay.com>
2025-03-20 18:29:09 +0100

    LPD-51800 Add AssetRenderer and Factory for SharingEntry

---
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/asset/model/SharingEntryAssetRenderer.java
@@ -0,0 +1,123 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sharing.web.internal.asset.model;
+
+import com.liferay.asset.kernel.model.BaseAssetRenderer;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.sharing.interpreter.SharingEntryInterpreter;
+import com.liferay.sharing.interpreter.SharingEntryInterpreterProvider;
+import com.liferay.sharing.model.SharingEntry;
+
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Mikel Lorza
+ */
+public class SharingEntryAssetRenderer extends BaseAssetRenderer<SharingEntry> {
+
+	public SharingEntryAssetRenderer(
+		SharingEntry sharingEntry,
+		SharingEntryInterpreterProvider sharingEntryInterpreterProvider) {
+
+		_sharingEntry = sharingEntry;
+		_sharingEntryInterpreterProvider = sharingEntryInterpreterProvider;
+	}
+
+	@Override
+	public SharingEntry getAssetObject() {
+		return _sharingEntry;
+	}
+
+	@Override
+	public String getClassName() {
+		return SharingEntry.class.getName();
+	}
+
+	@Override
+	public long getClassPK() {
+		return _sharingEntry.getSharingEntryId();
+	}
+
+	@Override
+	public long getGroupId() {
+		return _sharingEntry.getGroupId();
+	}
+
+	@Override
+	public String getSummary(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		try {
+			SharingEntryInterpreter sharingEntryInterpreter =
+				_sharingEntryInterpreterProvider.getSharingEntryInterpreter(
+					_sharingEntry);
+
+			if (sharingEntryInterpreter == null) {
+				return StringPool.BLANK;
+			}
+
+			return sharingEntryInterpreter.getAssetTypeTitle(
+				_sharingEntry, locale);
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+		}
+
+		return StringPool.BLANK;
+	}
+
+	@Override
+	public long getUserId() {
+		return _sharingEntry.getUserId();
+	}
+
+	@Override
+	public String getUserName() {
+		return _sharingEntry.getUserName();
+	}
+
+	@Override
+	public String getUuid() {
+		return _sharingEntry.getUuid();
+	}
+
+	@Override
+	public boolean include(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse, String template) {
+
+		return false;
+	}
+
+	@Override
+	public boolean isDisplayable() {
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SharingEntryAssetRenderer.class);
+
+	private final SharingEntry _sharingEntry;
+	private final SharingEntryInterpreterProvider
+		_sharingEntryInterpreterProvider;
+
+}
---
+++ modules/apps/sharing/sharing-web/src/main/java/com/liferay/sharing/web/internal/asset/model/SharingEntryAssetRendererFactory.java
@@ -0,0 +1,89 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.sharing.web.internal.asset.model;
+
+import com.liferay.asset.kernel.model.AssetRenderer;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.asset.kernel.model.BaseAssetRendererFactory;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.feature.flag.FeatureFlagManagerUtil;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.sharing.constants.SharingPortletKeys;
+import com.liferay.sharing.interpreter.SharingEntryInterpreterProvider;
+import com.liferay.sharing.model.SharingEntry;
+import com.liferay.sharing.service.SharingEntryLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Mikel Lorza
+ */
+@Component(
+	property = "javax.portlet.name=" + SharingPortletKeys.SHARING,
+	service = AssetRendererFactory.class
+)
+public class SharingEntryAssetRendererFactory
+	extends BaseAssetRendererFactory<SharingEntry> {
+
+	public static final String TYPE = "sharing_entry";
+
+	public SharingEntryAssetRendererFactory() {
+		setClassName(SharingEntry.class.getName());
+		setLinkable(true);
+		setPortletId(SharingPortletKeys.SHARING);
+	}
+
+	@Override
+	public AssetRenderer<SharingEntry> getAssetRenderer(long classPK, int type)
+		throws PortalException {
+
+		SharingEntry sharingEntry = _sharingEntryLocalService.getSharingEntry(
+			classPK);
+
+		if (!FeatureFlagManagerUtil.isEnabled(
+				sharingEntry.getCompanyId(), "LPD-17564")) {
+
+			return null;
+		}
+
+		return new SharingEntryAssetRenderer(
+			sharingEntry, _sharingEntryInterpreterProvider);
+	}
+
+	@Override
+	public String getClassName() {
+		return SharingEntry.class.getName();
+	}
+
+	@Override
+	public String getIconCssClass() {
+		return "sharingEntry";
+	}
+
+	@Override
+	public String getType() {
+		return TYPE;
+	}
+
+	@Override
+	public boolean isSearchable() {
+		if (FeatureFlagManagerUtil.isEnabled(
+				CompanyThreadLocal.getCompanyId(), "LPD-17564")) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private SharingEntryInterpreterProvider _sharingEntryInterpreterProvider;
+
+	@Reference
+	private SharingEntryLocalService _sharingEntryLocalService;
+
+}
