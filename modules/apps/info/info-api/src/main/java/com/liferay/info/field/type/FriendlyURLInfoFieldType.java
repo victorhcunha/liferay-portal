667b8fe38cf59600dbf0e77abf59c342da0a52c9
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2025-03-24 10:40:04 +0100

    LPD-50584 Add a dedicated InfoFieldType for the friendly URL

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/field/type/FriendlyURLInfoFieldType.java
@@ -0,0 +1,24 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.info.field.type;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class FriendlyURLInfoFieldType implements InfoFieldType {
+
+	public static final FriendlyURLInfoFieldType INSTANCE =
+		new FriendlyURLInfoFieldType();
+
+	@Override
+	public String getName() {
+		return "friendly-url";
+	}
+
+	private FriendlyURLInfoFieldType() {
+	}
+
+}
