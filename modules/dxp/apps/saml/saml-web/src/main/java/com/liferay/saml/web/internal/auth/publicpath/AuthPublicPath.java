e85d12803bd2c0af4b198b9fbdb74b7c16ee41cb
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-03-25 18:46:26 -0300

    LPD-49551 Delete the struts MetadataAction instead

--- modules/dxp/apps/saml/saml-web/src/main/java/com/liferay/saml/web/internal/auth/publicpath/AuthPublicPath.java
+++ modules/dxp/apps/saml/saml-web/src/main/java/com/liferay/saml/web/internal/auth/publicpath/AuthPublicPath.java
@@ -15,7 +15,6 @@ import org.osgi.service.component.annotations.Component;
 		"auth.public.path=/portal/saml/acs",
 		"auth.public.path=/portal/saml/auth_redirect",
 		"auth.public.path=/portal/saml/login",
-		"auth.public.path=/portal/saml/metadata",
 		"auth.public.path=/portal/saml/slo",
 		"auth.public.path=/portal/saml/slo_logout",
 		"auth.public.path=/portal/saml/slo_redirect",
