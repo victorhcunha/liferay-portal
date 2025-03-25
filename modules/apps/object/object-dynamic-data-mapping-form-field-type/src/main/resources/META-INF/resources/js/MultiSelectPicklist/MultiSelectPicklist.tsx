bae562ddf98b2747336502033e7b7159086eb61e
Pedro Leite <pedro.leite@liferay.com>
2025-03-25 19:50:14 -0300

    LPD-49643 Avoid formatting the value as JSON in the backend since the frontend expects an object

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/js/MultiSelectPicklist/MultiSelectPicklist.tsx
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/js/MultiSelectPicklist/MultiSelectPicklist.tsx
@@ -10,7 +10,7 @@ import {
 	MultipleSelection,
 	ReactFieldBase as FieldBase,
 } from 'dynamic-data-mapping-form-field-type';
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 
 interface MultiSelectOption {
 	label: string;
@@ -71,12 +71,24 @@ export default function MultiSelectPicklist({
 
 	const [localValues, setLocalValues] = useState(normalizedValue);
 
+	const onChangeRef = useRef(onChange);
+
+	useEffect(() => {
+		onChangeRef.current = onChange;
+	}, [onChange]);
+
+	useEffect(() => {
+		if (normalizedValue && normalizedValue.length) {
+			onChangeRef.current({target: {value: normalizedValue}});
+		}
+	}, [normalizedValue]);
+
 	const handleChange = (_: object, value: Values) => {
 		const updatedValues = localizedObjectField
 			? {...(value as LocalizedValue<string[]>)}
 			: [...(value as string[])];
 
-		onChange({target: {value: updatedValues}});
+		onChangeRef.current({target: {value: updatedValues}});
 
 		setLocalValues(updatedValues);
 	};
