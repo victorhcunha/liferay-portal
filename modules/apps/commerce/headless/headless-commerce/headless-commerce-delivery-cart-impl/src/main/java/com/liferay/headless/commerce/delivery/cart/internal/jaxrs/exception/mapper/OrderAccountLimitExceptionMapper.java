fd0651a3c15e4a4cfc07c5e0595cce37bcac2d04
Crescenzo Rega <crescenzo.rega@liferay.com>
2025-03-25 11:23:17 +0100

    LPD-52039 Add order account limit exception mapper

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-impl/src/main/java/com/liferay/headless/commerce/delivery/cart/internal/jaxrs/exception/mapper/OrderAccountLimitExceptionMapper.java
@@ -0,0 +1,53 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.headless.commerce.delivery.cart.internal.jaxrs.exception.mapper;
+
+import com.liferay.commerce.exception.CommerceOrderAccountLimitException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.vulcan.accept.language.AcceptLanguage;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Commerce.Delivery.Cart)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Commerce.Delivery.Cart.OrderAccountLimitExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class OrderAccountLimitExceptionMapper
+	extends BaseExceptionMapper<CommerceOrderAccountLimitException> {
+
+	@Override
+	protected Problem getProblem(
+		CommerceOrderAccountLimitException commerceOrderAccountLimitException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			_language.get(
+				_acceptLanguage.getPreferredLocale(),
+				"unable-to-create-a-new-order-as-the-open-order-limit-has-" +
+					"been-reached"));
+	}
+
+	@Context
+	private AcceptLanguage _acceptLanguage;
+
+	@Reference
+	private Language _language;
+
+}
