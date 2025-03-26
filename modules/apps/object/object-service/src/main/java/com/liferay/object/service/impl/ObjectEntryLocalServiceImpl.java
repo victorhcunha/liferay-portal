ec91303333c95b5d730b09b580323214a2ebe0a2
Guilherme Camacho <guilherme.camacho@liferay.com>
2025-03-26 14:32:31 -0300

    LPD-40689 Reuse logic

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -334,13 +334,9 @@ public class ObjectEntryLocalServiceImpl
 			tempDLFileEntries, null, user.isGuestUser(), groupId,
 			objectDefinition, serviceContext, true, userId, values);
 
-		for (Map.Entry<DLFileEntry, ObjectField> dlFileEntry :
-				tempDLFileEntries.entrySet()) {
-
-			_addFileEntry(
-				dlFileEntry.getKey(), objectDefinition, objectEntryId,
-				dlFileEntry.getValue(), serviceContext, userId, values);
-		}
+		_addFileEntries(
+			objectDefinition, objectEntryId, serviceContext, tempDLFileEntries,
+			userId, values);
 
 		Map<String, Serializable> insertedValues = new HashMap<>();
 
@@ -1659,13 +1655,9 @@ public class ObjectEntryLocalServiceImpl
 			objectEntry.getGroupId(), objectDefinition, serviceContext, true,
 			userId, values);
 
-		for (Map.Entry<DLFileEntry, ObjectField> dlFileEntry :
-				tempDLFileEntries.entrySet()) {
-
-			_addFileEntry(
-				dlFileEntry.getKey(), objectDefinition, objectEntryId,
-				dlFileEntry.getValue(), serviceContext, userId, values);
-		}
+		_addFileEntries(
+			objectDefinition, objectEntryId, serviceContext, tempDLFileEntries,
+			userId, values);
 
 		int workflowAction = serviceContext.getWorkflowAction();
 
@@ -2026,6 +2018,22 @@ public class ObjectEntryLocalServiceImpl
 	protected void modified(Map<String, Object> properties) {
 		_objectConfiguration = ConfigurableUtil.createConfigurable(
 			ObjectConfiguration.class, properties);
+	}
+
+	private void _addFileEntries(
+			ObjectDefinition objectDefinition, long objectEntryId,
+			ServiceContext serviceContext,
+			Map<DLFileEntry, ObjectField> tempDLFileEntries, long userId,
+			Map<String, Serializable> values)
+		throws PortalException {
+
+		for (Map.Entry<DLFileEntry, ObjectField> dlFileEntry :
+				tempDLFileEntries.entrySet()) {
+
+			_addFileEntry(
+				dlFileEntry.getKey(), objectDefinition, objectEntryId,
+				dlFileEntry.getValue(), serviceContext, userId, values);
+		}
 	}
 
 	private void _addFileEntry(
