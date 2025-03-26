2c2e78bcceef34d18826374a33106162b2759291
Luisa Novaes <luisanovaes.arq@gmail.com>
2025-03-26 14:50:05 -0300

    LRSD-8969 Update Client Extension to use Routes

---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/Routes.jsx
@@ -0,0 +1,28 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React, {Suspense} from 'react';
+
+const lazyRoutes = {
+	'lms-course-list': React.lazy(() =>	import('./components/Course/CoursesList')),
+	'lms-certification-list': React.lazy(() => import('./components/Certification/CertificationList')),
+	'lms-landingpage-view': React.lazy(() => import('./components/LandingPageView')),
+	'lms-learningpath-list': React.lazy(() => import('./components/LearningPath/LearningPathList')
+	),
+};
+
+export default function Routes({path, properties}) {
+	const Route = lazyRoutes[path];
+
+	if (!Route) {
+		return <h1>Page not found</h1>;
+	}
+
+	return (
+		<Suspense>
+			<Route properties={properties} />
+		</Suspense>
+	);
+}
--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/main.jsx
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/main.jsx
@@ -1,188 +1,38 @@
-
 /**
  * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {ClayIconSpriteContext} from '@clayui/icon';
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-
-import CertificationList from './components/Certification/CertificationList';
-import CoursesList from './components/Course/CoursesList';
-import LandingPageView from './components/Global/LandingPageView';
-import LearningPathsList from './components/LearningPath/LearningPathList';
-
+import { createRoot} from 'react-dom/client';
+import Routes from './Routes';
 import './index.scss';
 
-const ELEMENT_ID_CERTIFICATION_LIST = 'liferay-lms-certification-list';
-const ELEMENT_ID_COURSES_LIST = 'liferay-lms-courses-list';
-const ELEMENT_ID_LANDINGPAGE_VIEW = 'liferay-lms-landingpage-view';
-const ELEMENT_ID_LEARN_PATHS_LIST = 'liferay-lms-learn-paths-list';
-
-class CertificationListComponent extends HTMLElement {
-	constructor() {
-		super();
-		this.root = null;
-	}
-
+class WebComponent extends HTMLElement {
 	connectedCallback() {
 		if (!this.root) {
 			this.root = createRoot(this);
-		}
-		this.renderComponent();
-	}
 
-	renderComponent(
-		page = this.getAttribute('page') || 1,
-		pageSize = this.getAttribute('page-size') || 3,
-		paging = this.getAttribute('paging') || false,
-		durationFormat = this.getAttribute('duration-format')
-	) {
-		if (this.root) {
 			this.root.render(
-				<ClayIconSpriteContext.Provider value={Liferay.Icons.spritemap}>
-					<CertificationList
-						durationFormat={durationFormat}
-						page={page}
-						pageSize={pageSize}
-						paging={paging}
-					></CertificationList>
-				</ClayIconSpriteContext.Provider>
+				<Routes
+					path={this.getAttribute('path')}
+					properties={{
+						analyticsCloudURL:
+							this.getAttribute('analyticsCloudURL') || '',
+						cloudConsoleURL:
+							this.getAttribute('cloudConsoleURL') || '',
+						contactSupportURL:
+							this.getAttribute('contactSupportURL') || '',
+						eulaBaseURL: this.getAttribute('eulaBaseURL') || '',
+						marketoFormId: this.getAttribute('marketoFormId') || '',
+						trialEulaURL: this.getAttribute('trialEulaURL') || '',
+					}}
+				/>
 			);
 		}
 	}
 }
-class CoursesListComponent extends HTMLElement {
-	constructor() {
-		super();
-		this.root = null;
-	}
+const ELEMENT_ID = 'liferay-learn-custom-element';
 
-	connectedCallback() {
-		if (!this.root) {
-			this.root = createRoot(this);
-		}
-		this.renderComponent();
-	}
-
-	renderComponent(
-		page = this.getAttribute('page') || 1,
-		pageSize = this.getAttribute('page-size') || 3,
-		paging = this.getAttribute('paging') || false,
-		durationFormat = this.getAttribute('duration-format')
-	) {
-		if (this.root) {
-			this.root.render(
-				<ClayIconSpriteContext.Provider value={Liferay.Icons.spritemap}>
-					<CoursesList
-						durationFormat={durationFormat}
-						page={page}
-						pageSize={pageSize}
-						paging={paging}
-					></CoursesList>
-				</ClayIconSpriteContext.Provider>
-			);
-		}
-	}
-
-	disconnectedCallback() {
-		if (this.root) {
-			this.root.unmount();
-			this.root = null;
-		}
-	}
-}
-
-class LandingPageViewComponent extends HTMLElement {
-	constructor() {
-		super();
-		this.root = null;
-	}
-	connectedCallback() {
-		if (!this.root) {
-			this.root = createRoot(this);
-		}
-		this.renderComponent();
-	}
-	renderComponent(type = this.getAttribute('type')) {
-		if (this.root) {
-			this.root.render(
-				<ClayIconSpriteContext.Provider value={Liferay.Icons.spritemap}>
-					<LandingPageView type={type} />
-				</ClayIconSpriteContext.Provider>
-			);
-		}
-	}
-	disconnectedCallback() {
-		if (this.root) {
-			this.root.unmount();
-			this.root = null;
-		}
-	}
-}
-class LearningPathsListComponent extends HTMLElement {
-	constructor() {
-		super();
-		this.root = null;
-	}
-
-	connectedCallback() {
-		if (!this.root) {
-			this.root = createRoot(this);
-		}
-		this.renderComponent();
-	}
-
-	renderComponent(
-		page = this.getAttribute('page') || 1,
-		pageSize = this.getAttribute('page-size') || 3,
-		paging = this.getAttribute('paging') || false,
-		durationFormat = this.getAttribute('duration-format')
-	) {
-		if (this.root) {
-			this.root.render(
-				<ClayIconSpriteContext.Provider value={Liferay.Icons.spritemap}>
-					<LearningPathsList
-						durationFormat={durationFormat}
-						page={page}
-						pageSize={pageSize}
-						paging={paging}
-					></LearningPathsList>
-				</ClayIconSpriteContext.Provider>
-			);
-		}
-	}
-
-	disconnectedCallback() {
-		if (this.root) {
-			this.root.unmount();
-			this.root = null;
-		}
-	}
-}
-
-if (!customElements.get(ELEMENT_ID_CERTIFICATION_LIST)) {
-	customElements.define(
-		ELEMENT_ID_CERTIFICATION_LIST,
-		CertificationListComponent
-	);
-}
-
-if (!customElements.get(ELEMENT_ID_COURSES_LIST)) {
-	customElements.define(ELEMENT_ID_COURSES_LIST, CoursesListComponent);
-}
-
-if (!customElements.get(ELEMENT_ID_LANDINGPAGE_VIEW)) {
-	customElements.define(
-		ELEMENT_ID_LANDINGPAGE_VIEW,
-		LandingPageViewComponent
-	);
-}
-
-if (!customElements.get(ELEMENT_ID_LEARN_PATHS_LIST)) {
-	customElements.define(
-		ELEMENT_ID_LEARN_PATHS_LIST,
-		LearningPathsListComponent
-	);
+if (!customElements.get(ELEMENT_ID)) {
+	customElements.define(ELEMENT_ID, WebComponent);
 }
