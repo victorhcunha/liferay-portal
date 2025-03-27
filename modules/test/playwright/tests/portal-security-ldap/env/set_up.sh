ef675305cbad0faed5279f6cb727caedc7525438
ChrisKian <christopher.kian@liferay.com>
2025-03-27 13:13:24 -0700

    LPD-47223 Combine logging and execution into one function, only print if there is an error

--- modules/test/playwright/tests/portal-security-ldap/env/set_up.sh
+++ modules/test/playwright/tests/portal-security-ldap/env/set_up.sh
@@ -10,36 +10,21 @@ DEPENDENCIES_DIR_NAME=${CURRENT_DIR_NAME}/../dependencies
 
 echo DEPENDENCIES_DIR_NAME=${DEPENDENCIES_DIR_NAME}
 
-function print_command_status {
+function execute_command {
+	$1
+
 	if [ $? -ne 0 ]
 	then
 		echo "Command failed with exit status $?"
-	else
-		echo "Command succeeded"
 	fi
 }
 
 function ldap_set_up {
-	echo "Starting slapd:"
-	/usr/local/libexec/slapd -F /usr/local/etc/slapd.d
-
-	print_command_status
-
-	ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/exampleCompany.ldif -w "secret"
-
-	print_command_status
-
-	ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/admin.ldif -w "secret"
-
-	print_command_status
-
-	ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/addUsers.ldif -w "secret"
-
-	print_command_status
-
-	ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/addGroups.ldif -w "secret"
-
-	print_command_status
+	execute_command `/usr/local/libexec/slapd -F /usr/local/etc/slapd.d`
+	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/exampleCompany.ldif -w "secret"`
+	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/admin.ldif -w "secret"`
+	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/addUsers.ldif -w "secret"`
+	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/addGroups.ldif -w "secret"`
 }
 
 function main {
