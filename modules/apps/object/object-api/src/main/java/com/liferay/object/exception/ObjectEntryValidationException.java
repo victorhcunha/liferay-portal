401a3fb2ede5cbeca1d3a6c2d655d01a1f5e50a4
Jhosseph Gonzalez <jhossephgonzalez@gmail.com>
2025-03-27 16:59:41 -0300

    LPD-40689 Create ObjectEntryValidationException

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/ObjectEntryValidationException.java
@@ -0,0 +1,30 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.object.exception;
+
+import com.liferay.object.entry.validation.ValidationError;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.List;
+
+/**
+ * @author Jhosseph Gonzalez
+ */
+public class ObjectEntryValidationException extends PortalException {
+
+	public ObjectEntryValidationException(
+		List<ValidationError> validationErrors) {
+
+		_validationErrors = validationErrors;
+	}
+
+	public List<ValidationError> getValidationErrors() {
+		return _validationErrors;
+	}
+
+	private final List<ValidationError> _validationErrors;
+
+}
