29153d3908e1f3ef61a842906078cfd901854fff
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2025-03-27 09:18:24 +0100

    LPD-52161 Avoid null pointer exception if localizedValue doesn't exist

--- modules/apps/object/object-api/src/main/java/com/liferay/object/entry/util/ObjectEntryValuesUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/entry/util/ObjectEntryValuesUtil.java
@@ -17,6 +17,7 @@ import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlParserUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
 import java.io.Serializable;
@@ -92,7 +93,9 @@ public class ObjectEntryValuesUtil {
 				(Map<String, Object>)values.get(
 					objectField.getI18nObjectFieldName());
 
-			value = localizedValues.get(languageId);
+			if (MapUtil.isNotEmpty(localizedValues)) {
+				value = localizedValues.get(languageId);
+			}
 		}
 
 		if (objectField.compareBusinessType(
