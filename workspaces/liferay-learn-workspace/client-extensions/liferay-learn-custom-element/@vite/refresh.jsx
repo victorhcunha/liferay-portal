161b81406aa5b462ebbaf9ad96aff1db038d68cb
Luisa Novaes <luisanovaes.arq@gmail.com>
2025-03-27 15:24:25 -0300

    LRSD-8987 Create Custom Element Hot Reload

---
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/@vite/refresh.jsx
@@ -0,0 +1,11 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import RefreshRuntime from '/@react-refresh';
+
+RefreshRuntime.injectIntoGlobalHook(window);
+window.$RefreshReg$ = () => {};
+window.$RefreshSig$ = () => (type) => type;
+window.__vite_plugin_react_preamble_installed__ = true;
