9e8b21964f1f41eaf70bd919566e90f69ac66956
Manuele Castro <manuele.castro@liferay.com>
2025-03-27 14:28:14 -0300

    LPD-52015 Update test

--- modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-web-test/src/testIntegration/java/com/liferay/multi/factor/authentication/web/internal/portlet/action/test/LoginMVCActionCommandTest.java
+++ modules/dxp/apps/multi-factor-authentication/multi-factor-authentication-web-test/src/testIntegration/java/com/liferay/multi/factor/authentication/web/internal/portlet/action/test/LoginMVCActionCommandTest.java
@@ -15,6 +15,7 @@ import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.PortletApp;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.LiferayActionRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletConfig;
@@ -275,7 +276,27 @@ public class LoginMVCActionCommandTest {
 		throws Exception {
 
 		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
-			new MockLiferayPortletActionRequest();
+			new MockLiferayPortletActionRequest() {
+
+				@Override
+				public Portlet getPortlet() {
+					return _portletLocalService.getPortletById(
+						LoginPortletKeys.LOGIN);
+				}
+
+				@Override
+				public String getPortletName() {
+					return LoginPortletKeys.LOGIN;
+				}
+
+				{
+					Portlet portlet = getPortlet();
+
+					PortletApp portletApp = portlet.getPortletApp();
+
+					portletApp.setSpecMajorVersion(2);
+				}
+			};
 
 		mockLiferayPortletActionRequest.setAttribute(
 			JavaConstants.JAVAX_PORTLET_CONFIG, _getLiferayPortletConfig());
