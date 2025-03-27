0f0e0b5e9106dce78d86c6b6696f37f736473c86
ChrisKian <christopher.kian@liferay.com>
2025-03-27 13:13:51 -0700

    LPD-47223 Remove unused echo

--- modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
+++ modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
@@ -8,8 +8,6 @@ source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
 DEPENDENCIES_DIR_NAME=${CURRENT_DIR_NAME}/../dependencies
 
-echo DEPENDENCIES_DIR_NAME=${DEPENDENCIES_DIR_NAME}
-
 function main {
 	default_tear_down
 
