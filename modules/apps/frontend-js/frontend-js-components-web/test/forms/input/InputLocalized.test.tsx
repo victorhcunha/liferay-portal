0d57f75b039b3cd5e2d4b48813f245e3f2eaf040
Sandro Chinea <sandrochinea@gmail.com>
2025-03-28 09:45:01 +0000

    LPD-52263 Add tests

---
+++ modules/apps/frontend-js/frontend-js-components-web/test/forms/input/InputLocalized.test.tsx
@@ -0,0 +1,75 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import '@testing-library/jest-dom/extend-expect';
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+
+import {InputLocalized} from '../../../src/main/resources/META-INF/resources';
+
+describe('InputLocalized component', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+
+		(global as any).Liferay.Language.direction = {
+			en_US: 'rtl',
+		};
+	});
+
+	it('renders an input text control with a Clay language picker', () => {
+		render(
+			<InputLocalized
+				id="name"
+				label="Name"
+				onChange={() => null}
+				translations={{}}
+			/>
+		);
+
+		expect(screen.getByLabelText('Name')).toBeVisible();
+		expect(
+			screen.getByRole('button', {
+				name: 'en_US',
+			})
+		).toBeVisible();
+	});
+
+	it('renders an asterisk Clay icon when defined as "required"', () => {
+		const {container} = render(
+			<InputLocalized
+				id="name"
+				label="Name"
+				onChange={() => null}
+				required
+				translations={{}}
+			/>
+		);
+
+		expect(screen.getByLabelText('Namemandatory')).toBeVisible();
+		expect(
+			container.getElementsByClassName('lexicon-icon-asterisk')[0]
+		).toBeInTheDocument();
+	});
+
+	it('renders a validation error message when the "error" is defined', () => {
+		const {container} = render(
+			<InputLocalized
+				error="This field is required"
+				id="name"
+				label="Name"
+				onChange={() => null}
+				required
+				translations={{}}
+			/>
+		);
+
+		const errorMessage =
+			container.getElementsByClassName('form-feedback-item');
+
+		expect(screen.getByLabelText('Namemandatory')).toBeVisible();
+		expect(errorMessage[0]).toHaveAttribute('aria-live', 'assertive');
+		expect(errorMessage[0]).toHaveTextContent('This field is required');
+	});
+});
