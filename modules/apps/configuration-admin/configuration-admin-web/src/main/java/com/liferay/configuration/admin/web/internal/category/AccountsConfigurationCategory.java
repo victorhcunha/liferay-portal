129e5e7b8ac655fd53982dca76213ba961d18e95
Matyas Wollner <matyas.wollner@liferay.com>
2025-03-28 14:17:27 +0100

    LPD-51449 Move AccountsConfigurationCategory to the correct module

--- modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/category/AccountsConfigurationCategory.java
+++ modules/apps/configuration-admin/configuration-admin-web/src/main/java/com/liferay/configuration/admin/web/internal/category/AccountsConfigurationCategory.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-package com.liferay.account.internal.configuration.admin.category;
+package com.liferay.configuration.admin.web.internal.category;
 
 import com.liferay.configuration.admin.category.ConfigurationCategory;
 
