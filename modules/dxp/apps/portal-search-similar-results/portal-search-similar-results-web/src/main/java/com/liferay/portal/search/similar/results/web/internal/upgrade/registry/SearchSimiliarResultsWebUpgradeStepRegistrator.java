95d230f61e3c76e6564ed54cd4dd45d68894f7ca
Joshua Cords <joshua.cords@liferay.com>
2025-03-28 22:13:21 -0700

    LPD-49259 portal-search-similar-results-web: add SimilarResultsDisplay upgrade process
    https://liferay.atlassian.net/browse/LPD-49259

---
+++ modules/dxp/apps/portal-search-similar-results/portal-search-similar-results-web/src/main/java/com/liferay/portal/search/similar/results/web/internal/upgrade/registry/SearchSimiliarResultsWebUpgradeStepRegistrator.java
@@ -0,0 +1,49 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.portal.search.similar.results.web.internal.upgrade.registry;
+
+import com.liferay.portal.search.similar.results.web.internal.constants.SimilarResultsPortletKeys;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.portlet.display.template.upgrade.BaseUpgradePortletPreferences;
+
+import javax.portlet.PortletPreferences;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Joshua Cords
+ */
+@Component(service = UpgradeStepRegistrator.class)
+public class SearchSimiliarResultsWebUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		registry.registerInitialization();
+
+		registry.register(
+			"0.0.1", "0.0.2",
+			new BaseUpgradePortletPreferences() {
+
+				@Override
+				protected String[] getPortletIds() {
+					return new String[] {
+						SimilarResultsPortletKeys.SIMILAR_RESULTS +
+							"_INSTANCE_%"
+					};
+				}
+
+				@Override
+				protected void upgradePreferences(
+						long companyId, long ownerId, int ownerType, long plid,
+						String portletId, PortletPreferences portletPreferences)
+					throws Exception {
+				}
+
+			});
+	}
+
+}
