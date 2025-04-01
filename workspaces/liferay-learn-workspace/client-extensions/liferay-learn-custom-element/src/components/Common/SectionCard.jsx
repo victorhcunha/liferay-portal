809fb0b0b609bb646da27c2481b651cd43608439
Luisa Novaes <luisanovaes.arq@gmail.com>
2025-04-01 11:43:05 -0300

    LRSD-8949 Refactor

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/components/Common/SectionCard.jsx
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/components/Common/SectionCard.jsx
@@ -21,7 +21,7 @@ const SectionCard = ({
 	return (
 		<ClayLayout.Col className="d-flex course-layout-col" key={index} lg={4} md={6} sm={12} xl={4} xs={12}>
 			<a className="education-home-card-link" href={link} >
-				<div className="d-flex education-home-card-container learn-education__landing-card">
+				<div className="d-flex education-home-card-container">
 					<div className="card-content d-flex flex-column justify-content-between">
 						<h4 className="title">{title}</h4>
 						<div className="description">
