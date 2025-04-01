1345204e88209f63618bfa339584ee2edb74c748
Luisa Novaes <luisanovaes.arq@gmail.com>
2025-04-01 13:57:15 -0300

    LRSD-8949 Fix import

--- workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/services/course.js
+++ workspaces/liferay-learn-workspace/client-extensions/liferay-learn-custom-element/src/services/course.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-import {getCurrentSiteId} from '';
+import {getCurrentSiteId} from '../utils/util';
 
 import {config} from '../utils/constants';
 import {request} from '../utils/request';
