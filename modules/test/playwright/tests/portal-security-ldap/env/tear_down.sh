57d76507a5cb528b373392bc4652fa62181471f2
Rafael Praxedes <rafael.praxedes@liferay.com>
2025-04-02 17:54:49 +0200

    LPD-47223 SF

--- modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
+++ modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
@@ -8,17 +8,17 @@ source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
 DEPENDENCIES_DIR_NAME=${CURRENT_DIR_NAME}/../dependencies
 
-function main {
-	default_tear_down
-
-	ldap_tear_down
-}
-
 function ldap_tear_down {
 	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/removeGroups.ldif -w "secret"
 	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/removeUsers.ldif -w "secret"
 
 	kill -INT `cat /usr/local/var/run/slapd.pid`
+}
+
+function main {
+	default_tear_down
+
+	ldap_tear_down
 }
 
 main "${@}"
