615f51358fba05b5cf9a73a8abc14bab70ab40e1
ChrisKian <christopher.kian@liferay.com>
2025-04-03 11:12:46 -0700

    LPD-47223 SF, apply same changes in tear_down.sh too

--- modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
+++ modules/test/playwright/tests/portal-security-ldap/env/tear_down.sh
@@ -6,11 +6,11 @@ echo CURRENT_DIR_NAME=${CURRENT_DIR_NAME}
 
 source ${CURRENT_DIR_NAME}/../../../env/common.sh
 
-DEPENDENCIES_DIR_NAME=${CURRENT_DIR_NAME}/../dependencies
-
 function ldap_tear_down {
-	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/removeGroups.ldif -w "secret"
-	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/removeUsers.ldif -w "secret"
+	local dependencies_dir_name=${CURRENT_DIR_NAME}/../dependencies
+
+	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -f ${dependencies_dir_name}/removeGroups.ldif -w "secret"
+	ldapdelete -cx -D "cn=admin,dc=example,dc=com" -f ${dependencies_dir_name}/removeUsers.ldif -w "secret"
 
 	kill -INT `cat /usr/local/var/run/slapd.pid`
 }
