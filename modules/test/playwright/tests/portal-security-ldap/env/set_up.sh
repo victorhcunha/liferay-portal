84f16206e71bffce6e361097c6e4a5430a5a78f6
Brian Chan <brian.chan@liferay.com>
2025-04-03 12:32:00 +0200

    LPD-47223 SF

--- modules/test/playwright/tests/portal-security-ldap/env/set_up.sh
+++ modules/test/playwright/tests/portal-security-ldap/env/set_up.sh
@@ -9,7 +9,7 @@ source ${CURRENT_DIR_NAME}/../../../env/common.sh
 DEPENDENCIES_DIR_NAME=${CURRENT_DIR_NAME}/../dependencies
 
 function execute_command {
-	$1
+	${1}
 
 	if [ $? -ne 0 ]
 	then
@@ -19,10 +19,10 @@ function execute_command {
 
 function ldap_set_up {
 	execute_command `/usr/local/libexec/slapd -F /usr/local/etc/slapd.d`
-	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/exampleCompany.ldif -w "secret"`
-	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/admin.ldif -w "secret"`
-	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/addUsers.ldif -w "secret"`
 	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/addGroups.ldif -w "secret"`
+	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/addUsers.ldif -w "secret"`
+	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/admin.ldif -w "secret"`
+	execute_command `ldapadd -cx -D "cn=admin,dc=example,dc=com" -f ${DEPENDENCIES_DIR_NAME}/exampleCompany.ldif -w "secret"`
 }
 
 function main {
