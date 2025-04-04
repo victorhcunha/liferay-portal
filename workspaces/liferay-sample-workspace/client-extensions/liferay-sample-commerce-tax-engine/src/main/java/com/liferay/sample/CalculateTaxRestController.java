befd2eaa0234ccc521a8753e943f2cc6aa15c45d
Brian Chan <brian.chan@liferay.com>
2025-04-04 10:22:18 +0200

    LPD-47427 Remove duplicate BaseRestController and use the common one

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-tax-engine/src/main/java/com/liferay/sample/CalculateTaxRestController.java
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-tax-engine/src/main/java/com/liferay/sample/CalculateTaxRestController.java
@@ -5,9 +5,13 @@
 
 package com.liferay.sample;
 
+import com.liferay.client.extension.util.spring.boot3.BaseRestController;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.json.JSONObject;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -30,7 +34,6 @@ public class CalculateTaxRestController extends BaseRestController {
 		throws Exception {
 
 		log(jwt, _log, json);
-
 
 		return new ResponseEntity<>(
 			new JSONObject(
--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-tax-engine/src/main/java/com/liferay/sample/DescriptionRestController.java
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-commerce-tax-engine/src/main/java/com/liferay/sample/DescriptionRestController.java
@@ -5,9 +5,13 @@
 
 package com.liferay.sample;
 
+import com.liferay.client.extension.util.spring.boot3.BaseRestController;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import org.json.JSONObject;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
