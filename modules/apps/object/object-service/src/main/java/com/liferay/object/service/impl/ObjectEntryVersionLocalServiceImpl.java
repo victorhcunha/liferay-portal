80585a362dd7e6fa2a9346fdb00ac174f77906ea
victorhcunha <victor.cunha@liferay.com>
2025-04-10 10:56:14 -0300

    Revert "LPD-50256 Create method to delete versions of an object entry and apply to use"
    This reverts commit 8e7a21923ea0fe46d05acf7756fb791662b7c38c.

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryVersionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryVersionLocalServiceImpl.java
@@ -44,11 +44,6 @@ public class ObjectEntryVersionLocalServiceImpl
 	}
 
 	@Override
-	public void deleteObjectEntryVersions(long objectEntryId) {
-		objectEntryVersionPersistence.removeByObjectEntryId(objectEntryId);
-	}
-
-	@Override
 	public ObjectEntryVersion getObjectEntryVersion(
 			long objectEntryId, int version)
 		throws PortalException {
