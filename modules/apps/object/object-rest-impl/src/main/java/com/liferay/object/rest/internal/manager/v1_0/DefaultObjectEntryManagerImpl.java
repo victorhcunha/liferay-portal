abd2a836e140f252a41807db5a1c8707f138ba87
victorhcunha <victor.cunha@liferay.com>
2025-04-10 10:56:18 -0300

    Revert "LPD-50256 Create an endpoint to copy an specific snapshot of an object entry"
    This reverts commit 42cb43c4e20d78a8f959d5c7e9b1248ac1965fe5.

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/manager/v1_0/DefaultObjectEntryManagerImpl.java
@@ -20,7 +20,6 @@ import com.liferay.object.field.attachment.AttachmentManager;
 import com.liferay.object.field.business.type.ObjectFieldBusinessType;
 import com.liferay.object.field.business.type.ObjectFieldBusinessTypeRegistry;
 import com.liferay.object.field.setting.util.ObjectFieldSettingUtil;
-import com.liferay.object.field.util.ObjectFieldUtil;
 import com.liferay.object.model.ObjectAction;
 import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
@@ -50,7 +49,6 @@ import com.liferay.object.service.ObjectActionLocalService;
 import com.liferay.object.service.ObjectDefinitionLocalService;
 import com.liferay.object.service.ObjectEntryService;
 import com.liferay.object.service.ObjectEntryVersionService;
-import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.object.service.ObjectRelationshipService;
 import com.liferay.object.system.SystemObjectDefinitionManager;
@@ -823,33 +821,6 @@ public class DefaultObjectEntryManagerImpl
 		return updateObjectEntry(
 			dtoConverterContext, objectDefinition, objectEntryId,
 			existingObjectEntry);
-	}
-
-	@Override
-	public ObjectEntry restoreObjectEntryByVersion(
-			DTOConverterContext dtoConverterContext,
-			ObjectDefinition objectDefinition, long objectEntryId,
-			Integer version)
-		throws Exception {
-
-		return _restoreVersionedObjectEntry(
-			dtoConverterContext, objectDefinition,
-			getObjectEntryByVersion(
-				dtoConverterContext, objectEntryId, version));
-	}
-
-	@Override
-	public ObjectEntry restoreObjectEntryByVersion(
-			DTOConverterContext dtoConverterContext,
-			String externalReferenceCode, ObjectDefinition objectDefinition,
-			Integer version)
-		throws Exception {
-
-		return _restoreVersionedObjectEntry(
-			dtoConverterContext, objectDefinition,
-			getObjectEntryByVersion(
-				dtoConverterContext, externalReferenceCode, objectDefinition,
-				version));
 	}
 
 	@Override
@@ -1742,31 +1713,6 @@ public class DefaultObjectEntryManagerImpl
 			primaryKey2, serviceContext);
 	}
 
-	private ObjectEntry _restoreVersionedObjectEntry(
-			DTOConverterContext dtoConverterContext,
-			ObjectDefinition objectDefinition, ObjectEntry objectEntry)
-		throws Exception {
-
-		Map<String, Object> properties = objectEntry.getProperties();
-
-		List<ObjectField> objectFields =
-			_objectFieldLocalService.getCustomObjectFields(
-				objectDefinition.getObjectDefinitionId());
-
-		for (ObjectField objectField : objectFields) {
-			if (ObjectFieldUtil.isReadOnly(
-					ddmExpressionFactory, objectField,
-					objectEntry.getProperties())) {
-
-				properties.remove(objectField.getName());
-			}
-		}
-
-		return updateObjectEntry(
-			dtoConverterContext, objectDefinition, objectEntry.getId(),
-			objectEntry);
-	}
-
 	private Date _toDate(Locale locale, String valueString) {
 		if (Validator.isNull(valueString)) {
 			return null;
@@ -2087,9 +2033,6 @@ public class DefaultObjectEntryManagerImpl
 
 	@Reference
 	private ObjectFieldBusinessTypeRegistry _objectFieldBusinessTypeRegistry;
-
-	@Reference
-	private ObjectFieldLocalService _objectFieldLocalService;
 
 	@Reference
 	private ObjectRelatedModelsProviderRegistry
