1f1d5cd743d02a63bb3fad87bfe8abc633c85165
victorhcunha <victor.cunha@liferay.com>
2025-06-11 16:04:36 -0300

    Reapply "LPD-49265 portal-search-rest-test & users-admin-test: Fix SearchResultResourceTest and UserIndexerTest using "startsWith" matching, as the vendor string is "Elasticsearch (Sidecar)""
    This reverts commit 68abcbbc14484694746e5cdf6e540afe9dbf7d9c.

--- modules/apps/portal-search/portal-search-rest-test/src/testIntegration/java/com/liferay/portal/search/rest/resource/v1_0/test/SearchResultResourceTest.java
+++ modules/apps/portal-search/portal-search-rest-test/src/testIntegration/java/com/liferay/portal/search/rest/resource/v1_0/test/SearchResultResourceTest.java
@@ -809,7 +809,7 @@ public class SearchResultResourceTest extends BaseSearchResultResourceTestCase {
 	}
 
 	private boolean _isSearchEngineElasticsearch() {
-		return Objects.equals(
+		return StringUtil.startsWith(
 			_searchEngineInformation.getVendorString(), "Elasticsearch");
 	}
 
