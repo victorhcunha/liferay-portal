b9024b6b1aae7adbf7f3f21818a5fcc32bd81e6e
victorhcunha <victor.cunha@liferay.com>
2025-12-11 13:30:29 -0300

    Revert "LPD-71333 SF, adapt OSGi configuration names."
    This reverts commit 853588d7a72853e1c7f49d92756d0e0641b761c5.

--- modules/apps/data-cleanup/data-cleanup-impl/src/main/java/com/liferay/data/cleanup/internal/upgrade/executor/DataCleanupExecutor.java
+++ modules/apps/data-cleanup/data-cleanup-impl/src/main/java/com/liferay/data/cleanup/internal/upgrade/executor/DataCleanupExecutor.java
@@ -109,7 +109,7 @@ public class DataCleanupExecutor {
 			dataCleanupConfiguration::cleanUpDirectoryModuleData,
 			"com.liferay.directory.web", DirectoryUpgradeProcess::new);
 		_cleanUpModuleData(
-			dataCleanupConfiguration::cleanUpFrontendImageEditorModuleData,
+			dataCleanupConfiguration::cleanUpImageEditorModuleData,
 			"com.liferay.frontend.image.editor.web",
 			FrontendImageEditorUpgradeProcess::new);
 		_cleanUpModuleData(
@@ -170,12 +170,12 @@ public class DataCleanupExecutor {
 			"com.liferay.social.group.statistics.web",
 			SocialGroupStatisticsUpgradeProcess::new);
 		_cleanUpModuleData(
-			dataCleanupConfiguration::cleanUpSocialPrivateMessagingModuleData,
+			dataCleanupConfiguration::cleanUpPrivateMessagingModuleData,
 			"com.liferay.social.privatemessaging.service",
 			() -> new SocialPrivateMessagingUpgradeProcess(
 				_mbThreadLocalService));
 		_cleanUpModuleData(
-			dataCleanupConfiguration::cleanUpSocialRequestsModuleData,
+			dataCleanupConfiguration::cleanUpSocialRequestModuleData,
 			"com.liferay.social.requests.web",
 			SocialRequestsUpgradeProcess::new);
 		_cleanUpModuleData(
