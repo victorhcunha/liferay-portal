024728cacafc70369e0d988e7e86279e59932dd3
victorhcunha <victor.cunha@liferay.com>
2025-12-18 11:19:31 -0300

    Revert "LPD-52709 Remove dead code"
    This reverts commit 84957dcbabc0d9f3526e363ab204f7ad3a60e160.

---
+++ modules/apps/frontend-js/frontend-js-importmaps-extender/src/main/java/com/liferay/frontend/js/importmaps/extender/internal/configuration/JSImportMapsConfiguration.java
@@ -0,0 +1,33 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.js.importmaps.extender.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@ExtendedObjectClassDefinition(category = "infrastructure", generateUI = false)
+@Meta.OCD(
+	description = "frontend-js-import-maps-description",
+	id = "com.liferay.frontend.js.importmaps.extender.internal.configuration.JSImportMapsConfiguration",
+	localization = "content/Language",
+	name = "frontend-js-import-maps-configuration-name"
+)
+public interface JSImportMapsConfiguration {
+
+	@Meta.AD(deflt = "true", name = "enable-import-maps", required = false)
+	public boolean enableImportMaps();
+
+	@Meta.AD(
+		deflt = "false", description = "enable-es-module-shims-help",
+		name = "enable-es-module-shims", required = false
+	)
+	public boolean enableESModuleShims();
+
+}
