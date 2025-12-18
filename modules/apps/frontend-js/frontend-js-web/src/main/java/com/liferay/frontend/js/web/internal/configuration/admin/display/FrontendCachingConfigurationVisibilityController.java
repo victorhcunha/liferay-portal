613915ab8e40233abe837f3a4dfbc5d4895b57bf
victorhcunha <victor.cunha@liferay.com>
2025-12-18 11:19:38 -0300

    Revert "LPD-69958 Undo "Hide JS configuration options for the next release of DXP (since it is currently disabled)""
    This reverts commit b554169da1f763b872830ac160c018a712d1a2f2.

---
+++ modules/apps/frontend-js/frontend-js-web/src/main/java/com/liferay/frontend/js/web/internal/configuration/admin/display/FrontendCachingConfigurationVisibilityController.java
@@ -0,0 +1,31 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.frontend.js.web.internal.configuration.admin.display;
+
+import com.liferay.configuration.admin.display.ConfigurationVisibilityController;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+import java.io.Serializable;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Iván Zaera Avellón
+ */
+@Component(service = ConfigurationVisibilityController.class)
+public class FrontendCachingConfigurationVisibilityController
+	implements ConfigurationVisibilityController {
+
+	@Override
+	public boolean isVisible(
+		ExtendedObjectClassDefinition.Scope scope, Serializable scopePK) {
+
+		// LPD-69957
+
+		return false;
+	}
+
+}
