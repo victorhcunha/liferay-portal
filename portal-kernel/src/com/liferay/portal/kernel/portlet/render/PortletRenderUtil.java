2c8cbabb9c4dbcc471b488f9e2d9aad988799e11
victorhcunha <victor.cunha@liferay.com>
2025-12-18 11:19:20 -0300

    Revert "LPD-52709 Use hashed URLs for portlet's JS files"
    This reverts commit 2b69fa1fb479b00c285717d88a8c6151eea29e81.

--- portal-kernel/src/com/liferay/portal/kernel/portlet/render/PortletRenderUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/portlet/render/PortletRenderUtil.java
@@ -6,6 +6,7 @@
 package com.liferay.portal.kernel.portlet.render;
 
 import com.liferay.petra.io.StreamUtil;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.content.security.policy.ContentSecurityPolicyNonceProviderUtil;
 import com.liferay.portal.kernel.frontend.hashed.files.HashedFilesRegistryUtil;
@@ -14,6 +15,7 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.LayoutTypePortlet;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.Theme;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.HtmlUtil;
@@ -44,6 +46,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Predicate;
 
 /**
  * @author Iván Zaera Avellón
@@ -193,6 +196,110 @@ public class PortletRenderUtil {
 		return allPortlets;
 	}
 
+	private static List<String> _getComboServletURLs(
+		Collection<PortletResourceAccessor> portletResourceAccessors,
+		Collection<Portlet> portlets, Predicate<String> predicate,
+		long timestamp, String urlPrefix, Set<String> visitedURLs) {
+
+		if (predicate == null) {
+			predicate = s -> true;
+		}
+
+		List<String> urls = new ArrayList<>();
+
+		StringBundler comboServletSB = new StringBundler();
+
+		for (Portlet portlet : portlets) {
+			for (PortletResourceAccessor portletResourceAccessor :
+					portletResourceAccessors) {
+
+				String contextPath = null;
+
+				if (portletResourceAccessor.isPortalResource()) {
+					contextPath = PortalUtil.getPathContext();
+				}
+				else {
+					contextPath =
+						PortalUtil.getPathProxy() + portlet.getContextPath();
+				}
+
+				Collection<String> portletResources =
+					portletResourceAccessor.get(portlet);
+
+				for (String portletResource : portletResources) {
+					if (!predicate.test(portletResource)) {
+						continue;
+					}
+
+					String prefix = null;
+
+					for (String specialPrefix : _specialPrefixes) {
+						if (portletResource.startsWith(specialPrefix)) {
+							portletResource = portletResource.substring(
+								specialPrefix.length());
+
+							prefix = specialPrefix;
+
+							break;
+						}
+					}
+
+					boolean absolute = HttpComponentsUtil.hasProtocol(
+						portletResource);
+
+					if (!absolute) {
+						portletResource = contextPath + portletResource;
+					}
+
+					if (Validator.isNotNull(prefix)) {
+						portletResource = prefix + portletResource;
+					}
+
+					if (visitedURLs.contains(portletResource)) {
+						continue;
+					}
+
+					visitedURLs.add(portletResource);
+
+					if (absolute || Validator.isNotNull(prefix)) {
+						urls.add(portletResource);
+					}
+					else {
+						comboServletSB.append(StringPool.AMPERSAND);
+
+						if (!portletResourceAccessor.isPortalResource()) {
+							comboServletSB.append(portlet.getPortletId());
+							comboServletSB.append(StringPool.COLON);
+						}
+
+						comboServletSB.append(
+							HtmlUtil.escapeURL(portletResource));
+
+						timestamp = Math.max(timestamp, portlet.getTimestamp());
+					}
+				}
+			}
+		}
+
+		if (comboServletSB.length() > 0) {
+			String url = urlPrefix + comboServletSB;
+
+			url = HttpComponentsUtil.addParameter(url, "t", timestamp);
+
+			urls.add(url);
+		}
+
+		return urls;
+	}
+
+	private static String _getMinifierType(URLType urlType) {
+		if (urlType == URLType.CSS) {
+			return "css";
+		}
+
+		return "js";
+	}
+
 	private static PortletRenderParts _getPortletRenderParts(
 		HttpServletRequest httpServletRequest, String portletHTML,
 		Portlet portlet, boolean portletOnLayout) {
@@ -327,15 +434,12 @@ public class PortletRenderUtil {
 								contextPath + portletResource, themeDisplay);
 						}
 						else if (urlType == URLType.JAVASCRIPT) {
-							portletResource = contextPath + portletResource;
+							Portlet rootPortlet = portlet.getRootPortlet();
 
-							String hashedFileURI =
-								HashedFilesRegistryUtil.getHashedFileURI(
-									portletResource);
-
-							if (hashedFileURI != null) {
-								portletResource = hashedFileURI;
-							}
+							portletResource = PortalUtil.getStaticResourceURL(
+								httpServletRequest,
+								contextPath + portletResource,
+								rootPortlet.getTimestamp());
 						}
 						else {
 							throw new UnsupportedOperationException(
@@ -384,10 +488,42 @@ public class PortletRenderUtil {
 
 		List<String> urls;
 
-		if ((urlType == URLType.CSS) || (urlType == URLType.JAVASCRIPT)) {
+		if (urlType == URLType.CSS) {
 			urls = _getStaticURLs(
 				httpServletRequest, portletResourceAccessors, portlets, urlType,
 				visitedURLs);
+		}
+		else if (urlType == URLType.JAVASCRIPT) {
+			ThemeDisplay themeDisplay =
+				(ThemeDisplay)httpServletRequest.getAttribute(
+					WebKeys.THEME_DISPLAY);
+
+			boolean fastLoad = themeDisplay.isThemeJsFastLoad();
+
+			if (fastLoad) {
+				Predicate<String> predicate =
+					resource -> !themeDisplay.isIncludedJs(resource);
+
+				Theme theme = themeDisplay.getTheme();
+
+				urls = _getComboServletURLs(
+					portletResourceAccessors, portlets, predicate,
+					theme.getTimestamp(),
+					PortalUtil.getStaticResourceURL(
+						httpServletRequest,
+						themeDisplay.getCDNDynamicResourcesHost() +
+							themeDisplay.getPathContext() + "/combo",
+						StringBundler.concat(
+							"minifierType=", _getMinifierType(urlType),
+							"&themeId=", themeDisplay.getThemeId()),
+						-1),
+					visitedURLs);
+			}
+			else {
+				urls = _getStaticURLs(
+					httpServletRequest, portletResourceAccessors, portlets,
+					urlType, visitedURLs);
+			}
 		}
 		else {
 			throw new UnsupportedOperationException(
