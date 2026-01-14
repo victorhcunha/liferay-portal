f2679e69dc2b1e58082954d40eff07ce19f34d22
shuyangzhou <shuyang.zhou@liferay.com>
2026-01-14 09:27:41 -0800

    LPD-X Add Oracle style comment query hints support for DSL. This can be used for Oracle / MySQL / MariaDB / Postgres (with pg_hint_plan extension). For other DBs, the hints will be seen as comment message and completely ignored by the DBMS.

--- modules/core/petra/petra-sql-dsl-spi/src/main/java/com/liferay/petra/sql/dsl/spi/query/Select.java
+++ modules/core/petra/petra-sql-dsl-spi/src/main/java/com/liferay/petra/sql/dsl/spi/query/Select.java
@@ -8,6 +8,7 @@ package com.liferay.petra.sql.dsl.spi.query;
 import com.liferay.petra.sql.dsl.ast.ASTNodeListener;
 import com.liferay.petra.sql.dsl.expression.Alias;
 import com.liferay.petra.sql.dsl.expression.Expression;
+import com.liferay.petra.sql.dsl.query.FromStep;
 import com.liferay.petra.sql.dsl.spi.ast.BaseASTNode;
 
 import java.util.Collection;
@@ -23,12 +24,20 @@ public class Select extends BaseASTNode implements DefaultFromStep {
 	public Select(
 		boolean distinct, Collection<? extends Expression<?>> expressions) {
 
-		_distinct = distinct;
-		_expressions = Objects.requireNonNull(expressions);
+		this(distinct, Objects.requireNonNull(expressions), null);
 	}
 
 	public Collection<? extends Expression<?>> getExpressions() {
 		return _expressions;
+	}
+
+	public String getHints() {
+		return _hints;
+	}
+
+	@Override
+	public FromStep hints(String hints) {
+		return new Select(_distinct, _expressions, hints);
 	}
 
 	public boolean isDistinct() {
@@ -39,11 +48,16 @@ public class Select extends BaseASTNode implements DefaultFromStep {
 	protected void doToSQL(
 		Consumer<String> consumer, ASTNodeListener astNodeListener) {
 
-		if (_distinct) {
-			consumer.accept("select distinct ");
+		consumer.accept("select ");
+
+		if (_hints != null) {
+			consumer.accept("/*+ ");
+			consumer.accept(_hints);
+			consumer.accept(" */ ");
 		}
-		else {
-			consumer.accept("select ");
+
+		if (_distinct) {
+			consumer.accept("distinct ");
 		}
 
 		if (_expressions.isEmpty()) {
@@ -75,7 +89,17 @@ public class Select extends BaseASTNode implements DefaultFromStep {
 		}
 	}
 
+	private Select(
+		boolean distinct, Collection<? extends Expression<?>> expressions,
+		String hints) {
+
+		_distinct = distinct;
+		_expressions = expressions;
+		_hints = hints;
+	}
+
 	private final boolean _distinct;
 	private final Collection<? extends Expression<?>> _expressions;
+	private final String _hints;
 
 }
