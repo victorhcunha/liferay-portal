6a454d08bbef6d8568dde53fac4060c50b2c5c88
Victor Kammerer <vko@cin.ufpe.com>
2026-05-14 11:51:16 -0300

    LPD-90360 Upgrade existing entries with picklist-as-state field

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/registry/ObjectServiceUpgradeStepRegistrator.java
@@ -703,6 +703,11 @@ public class ObjectServiceUpgradeStepRegistrator
 			"11.0.0", "12.0.0",
 			new com.liferay.object.internal.upgrade.v12_0_0.
 				ObjectFieldUpgradeProcess());
+
+		registry.register(
+			"12.0.0", "12.0.1",
+			new com.liferay.object.internal.upgrade.v12_0_1.
+				ObjectEntryPicklistDefaultValueUpgradeProcess());
 	}
 
 	@Reference
