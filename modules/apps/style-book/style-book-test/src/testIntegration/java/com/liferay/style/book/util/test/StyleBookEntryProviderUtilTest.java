10827b2e7e93e8c09eeb8212d5ca19c3716e591b
Victor Cunha <victor.cunha@liferay.com>
2026-06-05 15:26:30 -0300

    Revert "LPD-88081 Use PermissionCheckerMethodTestRule in StyleBookEntryProviderUtilTest"
    This reverts commit 3abd14f6bb01dd1f714d02884f42d61628380015.

--- modules/apps/style-book/style-book-test/src/testIntegration/java/com/liferay/style/book/util/test/StyleBookEntryProviderUtilTest.java
+++ modules/apps/style-book/style-book-test/src/testIntegration/java/com/liferay/style/book/util/test/StyleBookEntryProviderUtilTest.java
@@ -14,6 +14,8 @@ import com.liferay.layout.test.util.LayoutTestUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
 import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.test.TestInfo;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -25,7 +27,6 @@ import com.liferay.portal.test.rule.FeatureFlag;
 import com.liferay.portal.test.rule.FeatureFlags;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
-import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
 import com.liferay.style.book.model.StyleBookEntry;
 import com.liferay.style.book.service.StyleBookEntryLocalService;
 import com.liferay.style.book.util.StyleBookEntryProviderUtil;
@@ -48,12 +49,13 @@ public class StyleBookEntryProviderUtilTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new AggregateTestRule(
-			new LiferayIntegrationTestRule(),
-			PermissionCheckerMethodTestRule.INSTANCE);
+		new LiferayIntegrationTestRule();
 
 	@Before
 	public void setUp() throws Exception {
+		PermissionThreadLocal.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(TestPropsValues.getUser()));
+
 		_group = GroupTestUtil.addGroup();
 
 		LayoutSet layoutSet = _group.getPublicLayoutSet();
