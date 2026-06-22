e21effe8294086e3b4c271b2e0668e8208a82f45
Brian Chan <brian.chan@liferay.com>
2026-06-22 10:08:17 +0800

    LPD-93952 Only used here

--- modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
+++ modules/apps/object/object-rest-test/src/testIntegration/java/com/liferay/object/rest/internal/manager/v1_0/test/DefaultObjectEntryManagerImplTest.java
@@ -6093,19 +6093,11 @@ public class DefaultObjectEntryManagerImplTest
 				},
 				ObjectDefinitionConstants.SCOPE_COMPANY);
 
-		// Filtering the relationship's external reference code for an empty
-		// value must return the entries that are not linked through the
-		// relationship
-
-		_assertFilteredObjectEntryIds(
+		_testGetObjectEntriesFilterByEmptyRelationshipExternalReferenceCode(
 			buildEqualsExpressionFilterString(
 				_objectRelationshipERCObjectFieldName, StringPool.BLANK),
 			unlinkedObjectEntry.getId());
-
-		// Filtering by a linked external reference code must still return only
-		// the linked entry
-
-		_assertFilteredObjectEntryIds(
+		_testGetObjectEntriesFilterByEmptyRelationshipExternalReferenceCode(
 			buildEqualsExpressionFilterString(
 				_objectRelationshipERCObjectFieldName,
 				parentObjectEntry.getExternalReferenceCode()),
@@ -10630,7 +10622,7 @@ public class DefaultObjectEntryManagerImplTest
 			objectEntries.toString(), size, objectEntries.size());
 	}
 
-	private void _assertFilteredObjectEntryIds(
+	private void _testGetObjectEntriesFilterByEmptyRelationshipExternalReferenceCode(
 			String filterString, Long... expectedObjectEntryIds)
 		throws Exception {
 
