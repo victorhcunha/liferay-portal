6b8825233893eba2fedd63e69ab861d83e5347a6
Victor Cunha <victor.cunha@liferay.com>
2026-09-22 15:35:03 -0300

    LPD-103227 Revert CMS site initializer changes to fix compile error
    Reverts the SF, log-when-deferred, and initialize-CMS-site changes
    (cherry-picked from a24559b, 1086692, 16badce) on top of b5fd5c8495, which
    otherwise fails to compile due to a stray character left in
    SiteInitializerUtil.java by 815425a.

--- modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/util/SiteInitializerUtil.java
+++ modules/apps/site/site-cms-site-initializer-api/src/main/java/com/liferay/site/cms/site/initializer/util/SiteInitializerUtil.java
@@ -5,12 +5,7 @@
 
 package com.liferay.site.cms.site.initializer.util;
 
-import com.liferay.object.model.ObjectDefinition;
-import com.liferay.object.service.ObjectDefinitionLocalServiceUtil;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Layout;
@@ -44,28 +39,6 @@ public class SiteInitializerUtil {
 	public static void initialize(
 			long companyId, SiteInitializer siteInitializer)
 		throws PortalException {
-
-		for (String externalReferenceCode :
-				_EXTERNAL_REFERENCE_CODES) {
-
-			ObjectDefinition objectDefinition =
-				ObjectDefinitionLocalServiceUtil.
-					fetchObjectDefinitionByExternalReferenceCode(
-						externalReferenceCode, companyId);
-
-			if (objectDefinition == null) {
-				if (_log.isDebugEnabled()) {
-					_log.debug(
-						StringBundler.concat(
-							"Company ", companyId,
-							" is missing the CMS object definition ",
-							externalReferenceCode,
-							", so the CMS site initialization is deferred"));
-				}
-
-				return;
-			}
-		}
 
 		Group group = GroupLocalServiceUtil.getGroup(
 			companyId, GroupConstants.CMS);
@@ -147,14 +120,6 @@ public class SiteInitializerUtil {
 		}
 
 		return adminUsers.get(0);
-	}A
-
-	private static final String[] _EXTERNAL_REFERENCE_CODES = {
-		"L_CMS_BASIC_DOCUMENT", "L_CMS_BASIC_WEB_CONTENT", "L_CMS_BLOG",
-		"L_CMS_EXTERNAL_VIDEO"
-	};
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		SiteInitializerUtil.class);
+	}
 
 }
